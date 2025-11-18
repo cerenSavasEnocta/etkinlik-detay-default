b2bc9f663a43ff6c6eabe995196a4700b6de8626
cerenSavasEnocta <GitHub hesabndaki email>
2025-11-18 12:15:42 +0300

    fix(build): add React entry and tsconfig; resolve Vite entrypoint; successful build

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface AppProps {}
+interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+	render() {
+		return (
+			<div>
+				Etkinlik Detay Make
+			</div>
+		);
+	}
+}
+
+export default App;
+
+
